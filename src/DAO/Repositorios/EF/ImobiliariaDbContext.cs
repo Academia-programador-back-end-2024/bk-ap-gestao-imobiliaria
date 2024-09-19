8a91ad80892b8f0fcbd0d3b3573c2760d85e7d3d
George Hamann <george.hamann@gmail.com>
2024-09-19 17:45:26 -0300

    Configuração de banco por entidade

--- src/DAO/Repositorios/EF/ImobiliariaDbContext.cs
+++ src/DAO/Repositorios/EF/ImobiliariaDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Academia.Programador.Bk.Gestao.Imobiliaria.Dominio.ModuloCliente;
+﻿using Academia.Programador.Bk.Gestao.Imobiliaria.DAO.Repositorios.EF.Modulo_Cliente;
+using Academia.Programador.Bk.Gestao.Imobiliaria.Dominio.ModuloCliente;
 using Academia.Programador.Bk.Gestao.Imobiliaria.Web;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,19 +26,8 @@ public partial class ImobiliariaDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Cliente>(entity =>
-        {
-            entity.HasKey(e => e.ClienteId).HasName("PK__Clientes__71ABD0871A158499");
-
-            entity.HasIndex(e => e.Cpf, "UQ__Clientes__C1F897319F8FF376").IsUnique();
-
-            entity.Property(e => e.Cpf)
-                .HasMaxLength(11)
-                .HasColumnName("CPF");
-            entity.Property(e => e.Email).HasMaxLength(100);
-            entity.Property(e => e.Nome).HasMaxLength(100);
-            entity.Property(e => e.Telefone).HasMaxLength(20);
-        });
+        ClienteEntityConfiguration clienteEntityConfiguration = new();
+        modelBuilder.ApplyConfiguration(clienteEntityConfiguration);
 
         modelBuilder.Entity<Corretore>(entity =>
         {
